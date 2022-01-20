ff7c9da873716410d3bdee087fc559ec3270730d
divyamtalreja <divyamtalreja16@gmail.com>
2022-01-20 18:31:52 +0530

    done but error in tqdm

--- utils/model.py
+++ utils/model.py
@@ -4,6 +4,8 @@ from matplotlib.colors import ListedColormap
 import os
 import joblib
 import logging
+from tqdm import tqdm
+
 class Perceptron:
   def __init__(self, eta, epochs):
     self.weights = np.random.randn(3) * 1e-4 # SMALL WEIGHT INIT
@@ -23,7 +25,7 @@ class Perceptron:
     X_with_bias = np.c_[self.X, -np.ones((len(self.X), 1))] # CONCATINATION
     logging.info(f"X with bias: \n{X_with_bias}")
 
-    for epoch in range(self.epochs):
+    for epoch in tqdm(range(self.epochs),total=self.epochs,desc="training the model"):
       logging.info("--"*10)
       logging.info(f"for epoch: {epoch}")
       logging.info("--"*10)
